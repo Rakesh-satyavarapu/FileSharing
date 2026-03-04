11556abb42f11f73673a85b645da1e0d14b5ad4d
Rakesh-satyavarapu <rakeshsatyavarapu369@gmail.com>
2026-03-04 15:04:20 +0530

    config added

---
+++ Backend/src/main/java/com/fileshare/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.fileshare.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://fileshare-yy7x.onrender.com"
+                )
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+}
